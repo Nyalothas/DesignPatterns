7d77e41c9e9d3a974e3084befc0cccb7a2fad4a9
Ny'alotha <Nyalotha@outlook.com>
2017-10-04 12:09:53 +0300

    made IsPolicyASingleton test to pass

--- Singleton/Policy.cs
+++ Singleton/Policy.cs
@@ -2,7 +2,16 @@
 {
     public class Policy
     {
-        public static Policy Instance { get { return new Policy(); } }
+        private static Policy _instance;
+        public static Policy Instance {
+            get
+            {
+                if(_instance == null)
+                 _instance = new Policy();
+
+                return _instance;
+            }
+        }
 
         public int Id { get; set; }
         public string Insured { get; set; } = "Tim Bob";
