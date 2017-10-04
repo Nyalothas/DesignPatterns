77a5663d83ab7135135147193bdde4d53589a762
Ny'alotha <Nyalotha@outlook.com>
2017-10-04 11:59:43 +0300

    added the Policy class

---
+++ Singleton/Policy.cs
@@ -0,0 +1,9 @@
+﻿namespace Singleton
+{
+    class Policy
+    {
+        public int Id { get; set; }
+        public string Insured { get; set; } = "Tim Bob";
+        public string GetInsuredName() => Insured;
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Singleton
 {
@@ -10,6 +6,10 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            Policy Policy = new Policy();
+            string insuredName = Policy.GetInsuredName();
+
+            Console.WriteLine(insuredName);
         }
     }
 }
