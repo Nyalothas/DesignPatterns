f864826d89afeba799d5a5380f804589c7a065d6
Ny'alotha <Nyalotha@outlook.com>
2017-10-05 11:59:36 +0300

    added CarDecorator

---
+++ Decorator/Decorator/CarDecorator.cs
@@ -0,0 +1,18 @@
+﻿using Decorator.Component;
+
+namespace Decorator.Decorator
+{
+    class CarDecorator : Car
+    {
+        protected Car _car;
+        public CarDecorator(Car car)
+        {
+            _car = car;
+        }
+
+        public override double GetCarPrice() => _car.GetCarPrice();
+
+
+        public override string GetDescription() => _car.GetDescription();
+    }
+}
