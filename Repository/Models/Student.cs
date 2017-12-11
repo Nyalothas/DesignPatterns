25285e49591e54777055726a081800af6c62c63c
Ny'alotha <Nyalotha@outlook.com>
2017-12-11 13:00:13 +0200

    MVC Application for Repository Pattern

---
+++ Repository/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Repository.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Year { get; set; }
+    }
+}
