ee72075aff2b00675cbfd4295dfa5a7bf2a66250
Ny'alotha <Nyalotha@outlook.com>
2017-12-11 11:13:00 +0200

    Iterator Pattern

---
+++ Iterator/Iterator/IIterator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator.Iterator
+{
+    public interface IIterator
+    {
+        void First();           //  Sets current element to the first element
+        string Next();          //  Advances current to next element
+        bool IsDone();          //  Check if end of collection
+        string CurrentItem();   //  returns the current element
+    }
+}
